0c559db93681f6f0d61e8120fe88f967791daa0a
Xiangming Gao <gao_xiangming@hotmail.com>
2016-07-10 16:50:54 +0100

    add upper limit of iteration step

--- SolveEngine/Bisection.h
+++ SolveEngine/Bisection.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <cmath>
+#include <iostream>
 
 template<class T>
 double Bisection(double Target, double Low, double High, double Tolerance, T TheFunction)
-{
+{	
+	unsigned StepCounter = 0;
 	double x = 0.5*(Low + High);
 	double y = TheFunction(x);
 
@@ -14,6 +16,11 @@ double Bisection(double Target, double Low, double High, double Tolerance, T The
 			High = x;
 		x = 0.5*(Low + High);
 		y = TheFunction(x);
+		if (StepCounter > 500) {
+			std::cout << "\n Iteration steps > 500, the result may not right.\nplease reset lower and higher guesses.\n";
+			break;
+		}
+		++StepCounter;
 	} while ((fabs(y - Target) > Tolerance));
 
 	return x;	
